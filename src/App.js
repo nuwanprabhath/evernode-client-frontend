1b2100958875d65712cfec595a99b95841fe6cb5
nuwanprabhath <nuwanprabhath@gmail.com>
2024-03-22 19:41:44 +1100

    Adding NFT validation link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,65 @@
-import './App.css';
-import React, { useState } from 'react';
-import './App.css';
-import axios from 'axios';
+import "./App.css";
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
   const students = [
-    { name: 'John Doe', degree: 'Computer Science', date: '2022-05-01', studentId: '123' },
-    { name: 'Jane Smith', degree: 'Electrical Engineering', date: '2022-06-01', studentId: '456' },
-    // Add more students here
+    {
+      name: "John Doe",
+      degree: "Computer Science",
+      date: "2023-05-01",
+      studentId: "888066733",
+      studentAccount: "rJQWTGnJ6zYHtmymcohFCZAW7LqZEYMRrV",
+      uri: "514C3243573135393345",
+    },
+    {
+      name: "Jane Smith",
+      degree: "Electrical Engineering",
+      date: "2023-06-01",
+      studentId: "999066733",
+      studentAccount: "rJQWTGnJ6zYHtmymcohFCZAW7LqZEYMRrV",
+      uri: "4C545A42415331464D4F",
+    },
+    {
+      name: "Alice Johnson",
+      degree: "Physics",
+      date: "2023-07-01",
+      studentId: "066733",
+      studentAccount: "rJQWTGnJ6zYHtmymcohFCZAW7LqZEYMRrV",
+      uri: "474134364C364D594446",
+    },
+    {
+      name: "Bob Williams",
+      degree: "Mathematics",
+      date: "2023-08-01",
+      studentId: "4442454",
+      studentAccount: "rJQWTGnJ6zYHtmymcohFCZAW7LqZEYMRrV",
+      uri: "56523030454D53505952",
+    },
+    {
+      name: "Emily Davis",
+      degree: "Chemistry",
+      date: "2023-09-01",
+      studentId: "42454546",
+      studentAccount: "rJQWTGnJ6zYHtmymcohFCZAW7LqZEYMRrV",
+      uri: "56414F424B47584C314D",
+    },
+    {
+      name: "Michael Wilson",
+      degree: "Biology",
+      date: "2023-10-01",
+      studentId: "44454144",
+      studentAccount: "rJQWTGnJ6zYHtmymcohFCZAW7LqZEYMRrV",
+      uri: "3454514F31554E594E5A",
+    },
   ];
 
   const [selectedStudent, setSelectedStudent] = useState(null);
+  const [status, setStatus] = useState(null);
 
   const handleStudentChange = (event) => {
+    setStatus(null);
     const selectedStudentId = event.target.value;
     const student = students.find((s) => s.studentId === selectedStudentId);
     setSelectedStudent(student);
@@ -20,12 +67,16 @@ function App() {
 
   const handleGraduate = () => {
     if (selectedStudent) {
-      axios.post('http://localhost:4001/record', selectedStudent, { proxy: true })
+      setStatus("Graduating...");
+      axios
+        .post("http://localhost:4001/record", selectedStudent, { proxy: true })
         .then((response) => {
           console.log(response.data);
+          setStatus("Graduated!");
         })
         .catch((error) => {
           console.error(error);
+          setStatus("Error graduating");
         });
     }
   };
@@ -33,7 +84,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <select onChange={handleStudentChange}>
+        <div style={{ fontSize: "xxx-large" }}>World Greatest University</div>
+        <select
+          style={{ marginTop: "3em", fontSize: "x-large" }}
+          onChange={handleStudentChange}
+        >
           <option value="">Select a student</option>
           {students.map((student) => (
             <option key={student.studentId} value={student.studentId}>
@@ -42,20 +97,24 @@ function App() {
           ))}
         </select>
         {selectedStudent && (
-          <div>
+          <div style={{ textAlign: "left" }}>
             <p>Name: {selectedStudent.name}</p>
             <p>Degree: {selectedStudent.degree}</p>
             <p>Date: {selectedStudent.date}</p>
             <p>Student ID: {selectedStudent.studentId}</p>
           </div>
         )}
-        <button onClick={handleGraduate} disabled={!selectedStudent}>
+        <button
+          style={{ fontSize: "x-large" }}
+          onClick={handleGraduate}
+          disabled={!selectedStudent}
+        >
           Graduate
         </button>
+        <div>{status}</div>
       </header>
     </div>
   );
 }
-
 
 export default App;
--- src/Student.js
+++ src/Student.js
@@ -1,29 +1,132 @@
-import React, { useState } from "react";
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import QRCode from "react-qr-code";
+import "./index.css";
+import axios from "axios";
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+const validate = urlParams.get("validate");
+const API_HOST = process.env.API_HOST || "http://localhost:4001";
+const APP_HOST = window.location.origin;
+const validateUrl = `${APP_HOST}/student/?id=${id}&validate=true`;
 
 function Student() {
-  const [uri, setUri] = useState("");
+  const [data, setData] = useState("");
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    handleGetRecords(id);
+  }, []);
+
+  console.log("id: ", id);
+  console.log("status: ", status);
+  console.log("validate: ", validate);
+
+  function getStudentData() {
+    if (data) {
+      return (
+        <div>
+          <p>Name: {data.name}</p>
+          <p>Degree: {data.degree}</p>
+          <p>Graduation Date: {data.date}</p>
+          <p>Student ID: {data.studentId}</p>
+          <p>Student Account: {data.studentAccount}</p>
+        </div>
+      );
+    }
+  }
+
+  function getQRCode() {
+    if (!validate) {
+      return <div>
+        <div style={{ height: "auto", margin: "0 auto" }}>
+          <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "90%", width: "90%" }}
+            value={validateUrl}
+            viewBox={`0 0 256 256`}
+          />
+        </div>
+        <a href={validateUrl} target="_blank" rel="noopener noreferrer" style={{color: "#8ecff1"}}>
+          Student record QR Code
+        </a>
+      </div>;
+    }
+  }
+
+  function getNftLink() {
+    if (!validate) {
+      const qrCodeUrl = `https://test.xahauexplorer.com/explorer/${data.nftHash}`;
+      return <div>
+        <div style={{ height: "auto", margin: "0 auto" }}>
+          <QRCode
+            size={256}
+            style={{ height: "auto", maxWidth: "90%", width: "90%" }}
+            value={qrCodeUrl}
+            viewBox={`0 0 256 256`}
+          />
+        </div>
+        <a href={qrCodeUrl} target="_blank" rel="noopener noreferrer" style={{color: "#8ecff1"}}>
+          NFT QR Code Link
+        </a>
+      </div>;
+    }
+  }
+
+  function getContent() {
+    if (status === "SUCCESS") {
+      return <>
+      {getStudentData()}
+      {getQRCode()}
+      {/* {getNftLink()} */}
+      </>;
+    } else if (status === "ERROR" && validate) {
+      return <div>No records found</div>;
+    } else if (
+      status === "ERROR" &&
+      (validate === null || validate === undefined)
+    ) {
+      return <div>You haven't graduated yet. Work hard though!</div>;
+    } else {
+      return <div>Loading...</div>;
+    }
+  }
 
   return (
     <div>
-      <h1>Student interface</h1>
-      <input
-        type="text"
-        id="uriInput"
-        value={uri}
-        onChange={(e) => setUri(e.target.value)}
-      />
-      <button onClick={handleGetRecords}>Get Records</button>
+      <header className="App-header">
+        <div style={{ fontSize: "xxx-large" }}>
+          Student Record {validate && " Check"}
+        </div>
+        {getContent()}
+        {validate && <a href={`https://test.xahauexplorer.com/explorer/${data.nftHash}`} target="_blank" rel="noopener noreferrer" style={{color: "#8ecff1"}}>
+          Validate NFT
+        </a>}
+        {/* <input
+          type="text"
+          id="uriInput"
+          value={uri}
+          onChange={(e) => setUri(e.target.value)}
+        /> */}
+        {/* <button onClick={handleGetRecords}>Get Records</button> */}
+      </header>
     </div>
   );
-  function handleGetRecords() {
-    const url = `http://localhost:4001/retrieve/?id=${uri}`;
-    axios.get(url)
+
+  function handleGetRecords(uri) {
+    const url = `${API_HOST}/retrieve/?id=${uri}`;
+    axios
+      .get(url)
       .then((response) => {
         const data = response.data;
         console.log("Data received: ", data);
+        setData(data.data);
+        setStatus("SUCCESS");
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error);
+        setStatus("ERROR");
+      });
   }
 }
 
